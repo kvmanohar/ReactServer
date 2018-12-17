be3eed9c58ceda0b1ec68e3c3db9b32fe89c9118
Manohar Kurapati <kvmanohar@gmail.com>
2018-12-17 21:32:19 +0000

    successfully saved the profile id to MongoDb

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const keys = require('./config/keys');
 const experss = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
 //Connect to mongoDB
 mongoose.connect(keys.mongoUri);
 
+//Create an Express app and pass it to routes.
 const app = experss();
-
 require('./routes/authRoutes')(app);
 
 //Dynamic Port Binding
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,10 @@ const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 
+//Load mongoose and get user collection
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
 //Inform PassportJS that we are going to using Google Strategy and pass client id and secret.
 passport.use(
 	new googleStrategy(
@@ -11,6 +15,9 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
+			new User({
+				googleId: profile.id
+			}).save();
 			console.log('accessToken:', accessToken);
 			console.log('refresh token:', refreshToken);
 			console.log('profile:', profile);
