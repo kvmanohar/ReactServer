93aef897980ee94d77b5dbfdbc904ce61e1b540f
Manohar Kurapati <kvmanohar@gmail.com>
2018-12-18 22:04:00 +0000

    Check user exists in DB and add if new user

--- services/passport.js
+++ services/passport.js
@@ -15,13 +15,14 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-			new User({
-				googleId: profile.id
-			}).save();
-			console.log('accessToken:', accessToken);
-			console.log('refresh token:', refreshToken);
-			console.log('profile:', profile);
-			console.log('done:', done);
+			User.findOne({ googleId: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					//user already exists
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id }).save().then((user) => done(null, user));
+				}
+			});
 		}
 	)
 );
